0cfa718e74c34222401a850ec5c38626ae107c13
20DaTo20 <datojavrishvili7@gmail.com>
2024-11-05 00:11:43 +0400

    project done

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+let btnElement = document.getElementById('buton')
+
+btnElement.addEventListener('click', function(){
+    alert('You Just Booked Your First Car')
+})
+let butonelement = document.getElementById('next-btn')
+
+butonelement.addEventListener('click', function(){
+    alert('You Subscribed Our News Letter')
+})
+let navelement = document.getElementById('b-nav')
+let burgerelement = document.getElementById('b-bar')
+
+    burgerelement.addEventListener('click', function(){
+    navelement.classList.toggle('nav-h')
+})
